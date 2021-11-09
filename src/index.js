3c95b4f681382d1aa4118284610a93ebf313e319
jefferson-s <jefferson.santos10@outlook.com>
2021-11-09 18:51:16 -0300

    linking imports and remove something

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { PokemonProvider } from "./providers/pokemon";
+
 ReactDOM.render(
   <React.StrictMode>
+    <PokemonProvider>
     <App />
+    </PokemonProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
