011c9e9d947b8df8ed84f594aa593d34fb251c9b
jefferson-s <jefferson.santos10@outlook.com>
2021-11-09 17:32:18 -0300

    create provider

--- src/providers/pokemon.js
+++ src/providers/pokemon.js
@@ -1,3 +1,13 @@
 import React, { createContext } from "react";
 
 export const PokemonContext = createContext();
+
+export const PokemonProvider = props =>{
+    const pokemons = [{ name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/"}]
+
+    return (
+        <PokemonContext.Provider value= {{ pokemons }}>
+            {props.children}
+        </PokemonContext.Provider>   
+    )
+};
