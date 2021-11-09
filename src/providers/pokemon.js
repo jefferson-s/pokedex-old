68859039bd750e49987063a460041614fc8e44c6
jefferson-s <jefferson.santos10@outlook.com>
2021-11-09 19:06:59 -0300

    using useState and useEffect and limiting pokemon

--- src/providers/pokemon.js
+++ src/providers/pokemon.js
@@ -1,9 +1,19 @@
-import React, { createContext } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import api from "../services/api";
 
 export const PokemonContext = createContext();
 
 export const PokemonProvider = props =>{
-    const pokemons = [{ name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/"}]
+    const [pokemons, setPokemons] = useState([]);
+    //function for fetch pokemons
+    const fetchPokemons = async () =>{
+        const {data} = await api.get('/pokemon?limit=18')
+        setPokemons(data.results);
+    };
+    //if the array is empty use the useEffect call function
+    useEffect(() => {
+        if(!pokemons.length) fetchPokemons();
+    }, []);
 
     return (
         <PokemonContext.Provider value= {{ pokemons }}>
